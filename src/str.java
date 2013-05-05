bb1633698b90c6457597629b9d92063c29bccd43
huangki <huangkik@gmail.com>
2013-05-05 15:50:47 +0800

    readfile

--- src/str.java
+++ src/str.java
@@ -1,10 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class str {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String s = in.next();
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File f = new File("C:\\input\\hw4_1.txt");
+		Scanner in = new Scanner(f);
+		String s ="";
+		while(in.hasNext()){
+			s= in.next();
+		}
+		 
 		String a = "";
 		boolean flag = true;
 		for (int i = 0; i < s.length(); i++) {
